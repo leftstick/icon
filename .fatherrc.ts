cd983c533638c35b2aad82a2e25005d57f140112
Howard.Zuo <leftstick@qq.com>
2020-04-30 11:31:19 +0800

    fix: make svg usable

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,7 +1,4 @@
-import svgr from '@svgr/rollup';
-
 export default {
   esm: 'rollup',
   cjs: 'rollup',
-  extraRollupPlugins: [svgr()],
 };
--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,8 @@
-
 ## Hello dumi!
+
+```jsx
+import React from 'react';
+import { Foo } from 'dumi-lib';
+
+export default () => <Foo />;
+```
